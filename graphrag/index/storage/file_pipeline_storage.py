f6a0dacc9f562a9a52483d749ba2d09b292cd9f7
EC2 Default User <ec2-user@ip-172-31-19-113.ap-northeast-1.compute.internal>
2024-07-10 09:18:40 +0000

    9

--- graphrag/index/storage/file_pipeline_storage.py
+++ graphrag/index/storage/file_pipeline_storage.py
@@ -66,7 +66,7 @@ class FilePipelineStorage(PipelineStorage):
     ) -> Any:
         """Get method definition."""
         file_path = join_path(self._root_dir, key)
-        response = await requests.post("http://10.2.230.41:8000/filestring4graph", json = {"directoryname": "393", "filename" : key})
+        response = requests.post("http://10.2.230.41:8000/filestring4graph", json = {"directoryname": "393", "filename" : key}).json()
         return response
         # if await self.has(key):
         #     return await self._read_file(file_path, as_bytes, encoding)
